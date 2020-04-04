58d6dddce20377406c5375ed94dda5e0dd00da15
edin93 <il_matador@hotmail.fr>
2020-04-04 02:44:22 +0100

    task 2 change puppet manifesto

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -4,31 +4,20 @@
 # Nginx is running on
 # ALL THIS MUST BE AUTOMATED BY DOING IT WITH PUPPET.
 exec { 'update_packages':
-path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-command  => 'sudo apt-get -y update',
-provider => 'shell',
-}
-
-exec { 'install_nginx':
-path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-command  => 'sudo apt-get -y install nginx',
-provider => 'shell',
+path    => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+command => 'sudo apt-get -y update',
 }
 
 exec { 'start_nginx':
 path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
 command  => 'sudo service nginx start',
 provider => 'shell',
-}
-
-exec { 'modify_nginx_config':
-path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin'],
-command  => 'sed -i -e "/sendfile/i \\\tadd_header X-Served-By \$hostname;" /etc/nginx/nginx.conf',
-provider => 'shell',
+unless   => 'sudo apt-get -y install nginx',
 }
 
 exec { 'restart_nginx':
 path     => ['/bin/', '/sbin/', '/usr/bin', '/usr/sbin'],
 command  => 'sudo service nginx restart',
 provider => 'shell',
+unless   => 'sed -i -e "/sendfile/i \\\tadd_header X-Served-By \$hostname;" /etc/nginx/nginx.conf',
 }
