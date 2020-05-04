8f8dfae1abc7520e9d3a8a8516062b4ee1f23699
edin93 <il_matador@hotmail.fr>
2020-05-04 20:56:24 +0100

    task 0 done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module contains a function that returns informations about a requested employee
+"""
+
+
+import requests
+from sys import argv
+import json
+
+
+url = "https://jsonplaceholder.typicode.com/users"
+user_id = argv[1]
+response = requests.get("{}/{}".format(url, user_id))
+if response.status_code == 200:
+    tasks_done_list = []
+    tasks_done = 0
+    all_tasks = 0
+    content = response.json()
+    todos = requests.get("{}/{}/todos".format(url, user_id))
+    todos = todos.json()
+    all_tasks = len(todos)
+    for task in todos:
+        if task.get("completed", None):
+            tasks_done += 1
+            tasks_done_list.append(task.get('title', ''))
+    print("{} is done with tasks({}/{})".format(
+        content.get('name', ""),
+        tasks_done,
+        all_tasks
+    ))
+    for task in tasks_done_list:
+        print(task)
