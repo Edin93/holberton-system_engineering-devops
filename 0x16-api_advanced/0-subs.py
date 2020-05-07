c3a18ab5b37e7914ffc30898724d0df836084a22
edin93 <il_matador@hotmail.fr>
2020-05-07 22:41:23 +0100

    task 0 changed all code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,10 +14,13 @@ def number_of_subscribers(subreddit):
     Parameters:
     subreddit (string): subreddit to search for.
     """
-    url = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
         'User-Agent': 'my custom user agent 1.0',
     }
     res = requests.get(url, headers=headers)
-    content = res.json()
-    return content.get('data', 0).get('subscribers', 0)
+    try:
+        content = res.json()
+        return content.get('data', 0).get('subscribers', 0)
+    except AttributeError:
+        return 0
