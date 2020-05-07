6ac712e406f5fe4e32ac86ee6ae191f6d0d4801d
edin93 <il_matador@hotmail.fr>
2020-05-08 00:53:59 +0100

    task 1 done

--- 0x16-api_advanced/1-backup.py
+++ 0x16-api_advanced/1-backup.py
@@ -5,7 +5,6 @@ first 10 hot posts listed for a given subreddit.
 """
 
 
-import json
 import requests
 
 
@@ -14,7 +13,7 @@ def top_ten(subreddit):
     Queries the Reddit API and prints the titles of the
     first 10 hot posts listed for a given subreddit.
     """
-    url = 'https://www.reddit.com/r/{}/new/.json'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
     headers = {
         'User-Agent': 'my custom user agent 1.0',
     }
@@ -22,11 +21,7 @@ def top_ten(subreddit):
     res = requests.get(url, headers=headers, params=params)
     try:
         content = res.json()
-        children = content.get('data').get('children')
-        if len(children) == 0:
-            print(None)
-        else:
-            for item in children:
-                print('{}'.format(item.get('data').get('title')))
+        for item in content.get('data').get('children'):
+            print('{}'.format(item.get('data').get('title')))
     except Exception as e:
         print(None)
