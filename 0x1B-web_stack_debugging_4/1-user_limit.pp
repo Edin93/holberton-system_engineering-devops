68e237f612d37c1086670d90c6b32d74967dbf9e
edin93 <il_matador@hotmail.fr>
2020-05-26 21:05:33 +0100

    task 1 done

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,18 @@
 # Configure the OS to allow the login with the holberton user.
-exec { 'update_ulimit':
+exec { 'update_holberton_hard':
 path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 200\"/g" /etc/default/nginx',
+command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 524288/" /etc/security/limits.conf',
 provider => 'shell',
 }
 
-exec { 'update_worker_processes':
+exec { 'update_holberton_soft':
 path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
-command  => 'sed -i "s/worker_processes 4;/worker_processes 8;/g" /etc/nginx/nginx.conf',
+command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 524288/" /etc/security/limits.conf',
+provider => 'shell',
+}
+
+exec { 'execute_the_updates':
+path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+command  => '/sbin/sysctl -p',
 provider => 'shell',
 }
